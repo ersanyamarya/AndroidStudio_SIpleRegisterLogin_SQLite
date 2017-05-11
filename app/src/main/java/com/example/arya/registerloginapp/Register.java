6193fcd23617df533a90b85699556f660fc9d7f1
Sanyam Arya <er.sanyam.arya@gmail.com>
2017-05-11 15:45:17 +0530

    some changer

--- app/src/main/java/com/example/arya/registerloginapp/Register.java
+++ app/src/main/java/com/example/arya/registerloginapp/Register.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+//Sanyam Arya
 public class Register extends Activity {
     EditText etName, etEmail, etPhone, etPassword;
     Button btSubmit, btLogin;
